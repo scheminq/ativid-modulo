fc10edfcdc45f477c2b4534c7266e181c2089217
scheminq <jeonslowyne@gmail.com>
2024-05-27 16:48:49 -0500

    arquivos

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//efeito do botão voltar ao Topo
+function topo()
+{
+    window.scrollTo
+    (
+        {
+            top: 0,
+            letft: 0,
+            behavior: 'smooth'
+        }
+    )
+}
+
+//Validação de Login
+
+function login()
+{
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+}
+
+
+//Ativar alert no botão cadastrar
